b7b07d0fca314e59edf1ce2259943ba3e12834d8
RandomServant <89282619+RandomServant@users.noreply.github.com>
2022-12-25 17:25:17 +1000

    Add Game.php

---
+++ front/Game.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css"></link>>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <script src="assets/js/script.js"></script>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <img class="header__logo" src="assets/images/LogoMini.png"></img>
+
+                <div class="nav">
+                    <a class="nav__link" href="#">Гараж</a>
+                    <div class="stick"></div>
+                    <a class="nav__link" href="#">Проекты</a>
+                    <div class="stick"></div>
+                    <a class="nav__link" href="#">Магазин</a>
+                </div>
+
+                <div class="users__icons">
+                    <a class = "user__box__link" href="#">
+                        <div class="icon">
+                            <div class="circle">
+                                <div class="circle__text">0</div>
+                            </div>
+                        </div>
+                    </a>
+                    <a class = "user__status__link__off" id="userStatus" href="#"></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="intro"></div>
+    <h1 class="name">Название</h1>
+
+    <div class="main__game">
+        <div class="whiteBlock">
+            <div class="in_white">
+                <div class="left_game">
+                    <h2 class="opis__text">Описание</h2>
+                    <div class="game__text" id="test">
+                        <span>
+                            Описание
+                        </span>
+                    </div>
+                    <div class="link__zone">
+                        <div class="itch" href="#"></div>
+                    </div>
+                </div>
+                <div class="right_game">
+                    <div class="imageGame"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="footer__inner">
+            <h1 class="footer__text"><span style="color: #E17521">Не</span> следите за нами</h1>
+            <div class="footer__links">
+                <a class="VK" href="#"></a>
+                <a class="TG" href="#"></a>
+            </div>
+            <div class="cat__light">
+                <div class="light"></div>
+                <div class="cat"></div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
