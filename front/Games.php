7355c41c4f5481f8a7aa1d5752487510c2a59ce5
RandomServant <89282619+RandomServant@users.noreply.github.com>
2022-12-27 10:59:08 +1000

    Add delete button

--- front/Games.php
+++ front/Games.php
@@ -11,7 +11,8 @@
              <?php foreach ($games as $game) { 
                 $img = "assets/images/".$game[3];?>
         <li class="table_el_g">
-            <a href="index.php?page=Game&game=<?=$game[1]?>"><img class="foto_g" src=<?=$img?>></img></a> 
+            <a class="delete"></a>
+            <a href="index.php?page=Game&game=<?=$game[1]?>"><img class="foto_g" src=<?=$img?>></img></a>
             <h3><?=$game[1]?></h3>
             <div class="buy">
                 <span class="podr__text" style="color: #E17521;">Подробнее</span>
--- front/Merch.php
+++ front/Merch.php
@@ -1,4 +1,4 @@
-<?php require_once "../setting/setting.php";?>
+<?php /*require_once "../setting/setting.php";*/?>
 
 <div class="ots"></div>
 
@@ -6,17 +6,18 @@
     <h1 class="merch__text">Маркет</h1>
     <ul class="table">
         <?php $result = mysqli_query($connect, " SELECT * FROM `product` ");
-            $product = $result->fetch_all(); ?>
-    <?php foreach ($product as $prod) { 
-        $img = "assets/images/".$prod[4];?>
-        <li class="table_el">
-            <a href=<?=$prod[5]?>><img class="foto" src=<?=$img?>></img></a> 
-            <h3><?=$prod[1]?></h3>
-            <div class="buy">
-                <span style="color: #E17521"><?=$prod[3]?> руб.</span>
-                <span class="buy__text" style="color: #E17521;">Купить</span>
-            </div>
-        </li>
+        $product = $result->fetch_all(); ?>
+        <?php foreach ($product as $prod) {
+            $img = "assets/images/".$prod[4];?>
+            <li class="table_el">
+                <a class="delete"></a>
+                <a href=<?=$prod[5]?>><img class="foto" src=<?=$img?>></img></a>
+                <h3><?=$prod[1]?></h3>
+                <div class="buy">
+                    <span style="color: #E17521"><?=$prod[3]?> руб.</span>
+                    <span class="buy__text" style="color: #E17521;">Купить</span>
+                </div>
+            </li>
         <? } ?>
     </ul>
 </div>
